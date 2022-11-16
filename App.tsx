6f281eaee452ed378e82e5c4b757e38d254dd1bd
Abhishek <akanth1994@gmail.com>
2022-11-16 19:16:21 +0530

    pop menu installed

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "./screens/Home";
 import { colors } from "./theme/colors";
+import { MenuProvider } from "react-native-popup-menu";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar
-        backgroundColor={colors.SlateGreyAccent}
-        barStyle="light-content"
-      />
-      <Home />
-    </SafeAreaProvider>
+    <MenuProvider>
+      <SafeAreaProvider>
+        <StatusBar
+          backgroundColor={colors.SlateGreyAccent}
+          barStyle="light-content"
+        />
+        <Home />
+      </SafeAreaProvider>
+    </MenuProvider>
   );
 }
